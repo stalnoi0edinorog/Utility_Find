62329184fbad33b9de9e8d7885c8dab8aa45a271
Petrunina Julia <petrunina.yua@edu.spbstu.ru>
2019-05-20 11:08:56 +0300

    1. Отредактирован Parser

--- src/parser.kt
+++ src/parser.kt
@@ -7,23 +7,25 @@ data class Parser(val args: Array<String>){
     var fileName = ""
 
     init {
+        when{
+            args.isEmpty() -> throw CmdLineFormatException("Empty line")
+            args.size > 4 -> throw CmdLineFormatException("Allowed command line size exceeded")
+            args.indexOf("-r") > 1 || args.indexOf("-d") > 1 -> throw CmdLineFormatException("Wrong argument order")
+        }
         if (args.size > 1){
              r = args.indexOf("-r") in 0..1
              d = args.indexOf("-d") in 0..1
         }
         when {
-            (r && d) -> index = 2
-            (!d && r) -> index = 0
-            (d && !r) -> index = 1
+            !r && !d && args.size > 1 || !d && args.size > 2  -> throw CmdLineFormatException("Number of arguments exceeded")
+            !d && r -> index = 0
+            d && !r -> index = 1
+            r && d -> index = 2
         }
-        when {
-            args.isEmpty() -> throw CmdLineFormatException("Empty line")
-            args.size > 4 -> throw CmdLineFormatException("Allowed command line size exceeded")
-            !r && !d && args.size > 1 || !d && args.size > 2 -> throw CmdLineFormatException("Number of arguments exceeded")
-            args.indexOf("-r") > 1 || args.indexOf("-d") > 1 -> throw CmdLineFormatException("Wrong argument order")
-            d && index + 1 == args.size -> throw CmdLineFormatException("Position argument is missing")
-        }
-        directory = if (index == 0 || index == -1) "." else args[index]
+        if(d && index + 1 == args.size )
+            throw CmdLineFormatException("Position argument is missing")
+
+        directory = if (!d) "." else args[index]
         fileName = args[index + 1]
     }
 }
